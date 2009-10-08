578adbf05e1edd134105dab9491c4a45d4c59aac
smly <eowner@gmail.com>
2009-10-08 22:19:47 +0900

    make LP (label propagation) class

--- label_propagation.cpp
+++ label_propagation.cpp
@@ -22,33 +22,33 @@ void showLabels(const graph::LabelMatrix& y_l, const graph::LabelMatrix& y_u,
                 const graph::Labels& lab)
 {
     const int N = L + U;
-    std::cout.setf(std::ios::fixed, std::ios::floatfield);
-    std::cout.precision(FLAGS_precision);
+    std::stringstream ss;
+    ss.setf(std::ios::fixed, std::ios::floatfield);
+    ss.precision(FLAGS_precision);
+
     if (FLAGS_showWeight) {
         for (int i=0; i<N; i++) {
             if (lab[i] >= 0) {
-                std::cout << "L: ";
+                ss << "L: ";
                 for (int c=0; c<C; c++) {
-                    if (c!=0) std::cout << " ";
-                    std::cout << y_l[c][node_index_v[i]];
+                    if (c!=0) ss << " ";
+                    ss << y_l[c][node_index_v[i]];
                 }
-                std::cout << std::endl;
+                ss << std::endl;
             } else {
-                std::cout << "U: ";
+                ss << "U: ";
                 for (int c=0; c<C; c++) {
-                    if (c!=0) std::cout << " ";
-                    std::cout << y_u[c][node_index_v[i]];
+                    if (c!=0) ss << " ";
+                    ss << y_u[c][node_index_v[i]];
                 }
-                std::cout << std::endl;
+                ss << std::endl;
             }
         }
     } else {
         for (int i=0; i<N; i++) {
             int argmax = 0;
             double argmax_val = -1.0;
-
             for (int c=0; c<C; c++) {
-                // get argmax
                 if (lab[i] >= 0) {
                     for (int c=0; c<C; c++) {
                         if (argmax_val < y_l[c][node_index_v[i]]) {
@@ -65,44 +65,72 @@ void showLabels(const graph::LabelMatrix& y_l, const graph::LabelMatrix& y_u,
                     }
                 }
             }
-
-            std::cout << argmax+1 << std::endl;
+            ss << argmax+1 << std::endl;
         }
     }
+    std::cout << ss.str();
 }
-int main (int argc, char** argv)
+
+class LP
 {
-    google::ParseCommandLineFlags(&argc, &argv, true);
-    google::InitGoogleLogging(argv[0]);
-
-    graph::Matrix trans_mat;
-    graph::Matrix norm_trans_mat;
+private:
+    unsigned int N; // size of nodes
+    unsigned int L; // size of labeled nodes
+    unsigned int U; // size of unlabeled nodes
+    unsigned int C;
+    graph::Matrix trans; // transition matrix
+    graph::Matrix norm; // row-normalized transition matrix
     graph::Labels labels;
+    graph::Matrix norm_uu; // UU part of norm
+    graph::Matrix norm_ul; // UL part of norm
+    graph::LabelMatrix y_u; // labels
+    graph::LabelMatrix y_l; // labels
+    std::vector<int> labeled_nodes, unlabeled_nodes;
+    std::vector<int> node_index_v;
+    double eps;
 
-    int max_lab;
+public:
+    void clear();
+    bool read (const std::string mfilename, const std::string lfilename);
+    bool prop (const int max_iteration);
+    bool write (const std::string filename);
+    void show ();
+
+    LP () : eps(1.0e-9), N(0), L(0), U(0) {};
+    LP (double eps) : eps(eps), N(0), L(0), U(0) {};
+    ~LP () { clear(); }
+};
+void LP::clear()
+{
+    // clear
+}
+bool LP::read (const std::string mfilename,
+               const std::string lfilename)
+{
     try {
-        graph::load_mat(trans_mat, norm_trans_mat, FLAGS_inputMatrix);
-        max_lab =  graph::load_lab(labels, FLAGS_inputLabels);
-    } catch (const std::exception &e) {
+        graph::load_mat(trans, norm, mfilename);
+        C = graph::load_lab(labels, lfilename);
+    } catch (const std::exception& e) {
         std::cout << "ERROR: " << e.what() << std::endl;
+        return false;
     }
-    assert(trans_mat.size() == labels.size());
-
-    const int N = trans_mat.size(); // 1 .. N
-    int labeled_cnt = 0;
+    assert(trans.size() == labels.size());
+    N = trans.size();
+    L = 0;
     std::vector<int> unlabeled;
     for (int i=0; i<N; i++) {
         if (labels[i] < 0) {
             unlabeled.pb(i+1);
         } else {
-            labeled_cnt++;
+            L++;
         }
     }
-    const int L = labeled_cnt;
-    const int U = N-L;
-    std::vector<int> labeled_nodes(L), unlabeled_nodes(U); // collection of node index
-    std::vector<int> node_index_v(N);
-    int l = 0, u = 0;
+    U = N - L;
+    labeled_nodes = std::vector<int>(L);
+    unlabeled_nodes = std::vector<int>(U);
+    node_index_v = std::vector<int>(N);
+
+    int l=0, u=0;
     for (int i=0; i<N; i++) {
         if (labels[i] < 0) {
             node_index_v[i] = u;
@@ -114,25 +142,27 @@ int main (int argc, char** argv)
     }
     assert(l==L);
     assert(u==U);
-    assert(static_cast<int>(unlabeled_nodes.size()) == U);
-    assert(static_cast<int>(labeled_nodes.size()) == L);
+    assert(static_cast<int>(unlabeled_nodes.size() == U));
+    assert(static_cast<int>(labeled_nodes.size() == L));
     assert(U == static_cast<int>(unlabeled.size()));
-    assert(L > 0);
-    assert(U > 0);
-    LOG(INFO) << "# of nodes: " << N;
-    LOG(INFO) << "# of labeled nodes: " << L;
-    LOG(INFO) << "# of unlabeled nodes: " << U;
 
-    graph::Matrix norm_trans_uu(U, graph::Array());
-    graph::Matrix norm_trans_ul(U, graph::Array());
-    load_submatrix(norm_trans_mat, norm_trans_uu, norm_trans_ul,
+    try {
+        if (N <= 0) throw std::runtime_error ("N is invalid.");
+        if (L <= 0) throw std::runtime_error ("L is invalid.");
+        if (U <= 0) throw std::runtime_error ("U is invalid.");
+        if (N != L + U) throw std::runtime_error ("matrix and labels are invalid.");
+    } catch (const std::exception& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        return false;
+    }
+
+    norm_uu = graph::Matrix(U, graph::Array());
+    norm_ul = graph::Matrix(U, graph::Array());
+    load_submatrix(norm, norm_uu, norm_ul,
                    U, L, unlabeled_nodes, labeled_nodes,
                    labels);
-
-    const int C = max_lab;
-    graph::LabelMatrix y_u(C, std::vector<double>(U, 0.0));
-    graph::LabelMatrix y_l(C, std::vector<double>(L, 0.0));
-
+    y_u = graph::LabelMatrix(C, std::vector<double>(U, 0.0));
+    y_l = graph::LabelMatrix(C, std::vector<double>(L, 0.0));
     for (unsigned int i=0; i<labeled_nodes.size(); i++) {
         const int node_index = labeled_nodes[i];
         assert(labels[node_index] != 0);
@@ -142,43 +172,91 @@ int main (int argc, char** argv)
         }
     }
 
-    const int max_iteration = FLAGS_iteration;
-    int iteration;
-    for (iteration = 0; iteration<max_iteration; iteration++) {
-        std::vector<std::vector<double> > y_ret(C, std::vector<double>(U, 0.0)); // Y'_u
+    return true;
+}
+bool LP::prop (const int max_iteration)
+{
+    try {
+        if (N <= 0) throw std::runtime_error ("N is invalid.");
+        if (L <= 0) throw std::runtime_error ("L is invalid.");
+        if (U <= 0) throw std::runtime_error ("U is invalid.");
+        if (N != L + U) throw std::runtime_error ("matrix and labels are invalid.");
+    } catch (const std::exception& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        return false;
+    }
+
+    std::cout << "Number of nodes:              " << N << std::endl;
+    std::cout << "Number of labeled nodes:      " << L << std::endl;
+    std::cout << "Number of unlabeled nodes:    " << U << std::endl;
+    std::cout << "eps:                          " << eps << std::endl;
+
+    for (int iter = 1; iter < max_iteration; iter++) {
         double err = 0.0;
-        // calc Y'_u = \overline{T}_{ul} Y_l
-        for (unsigned int i=0; i<unlabeled_nodes.size(); i++) {
-            for (int c=0; c<C; c++) {
-                const graph::Array arry = norm_trans_ul[i];
+        std::vector<std::vector<double> > y_ret(C, std::vector<double>(U, 0.0));
+        for (unsigned int i=0; i < U; i++) {
+            for (unsigned int c=0; c<C; c++) {
+                const graph::Array arry = norm_ul[i];
                 const int edge_sz = arry.size();
                 for (int j=0; j<edge_sz; j++) {
                     const double w = arry[j].weight;
                     const int src_index = arry[j].node - 1;
                     const double y_u_w = y_l[c][node_index_v[src_index]];
                     if (y_u_w < 1.0e-200 || w < 1.0e-200) continue;
-                    y_ret[c][i] += w * y_l[c][node_index_v[src_index]];
+                    y_ret[c][i] += w * y_u_w;
                 }
             }
         }
-        // calc Y'_u = \overline{T}_{uu} Y_u
-        for (unsigned int i=0; i<unlabeled_nodes.size(); i++) {
+
+        for (unsigned int i=0; i < U; i++) {
             for (int c=0; c<C; c++) {
-                const graph::Array arry = norm_trans_uu[i];
+                const graph::Array arry = norm_uu[i];
                 const int edge_sz = arry.size();
                 for (int j=0; j<edge_sz; j++) {
                     const double w = arry[j].weight;
                     const int src_index = arry[j].node - 1;
                     const double y_u_w = y_u[c][node_index_v[src_index]];
                     if (y_u_w < 1.0e-200 || w < 1.0e-200) continue;
-                    y_ret[c][i] += w * y_u[c][node_index_v[src_index]];
+                    y_ret[c][i] += w * y_u_w;
                 }
-                err += abs(y_ret[c][i] - y_u[c][i]);
+                err += y_ret[c][i] > y_u[c][i] ? y_ret[c][i]-y_u[c][i] : y_u[c][i]-y_ret[c][i];
             }
         }
+        if (iter % 400 == 1) {
+            std::cout << std::endl;
+            if (iter != 1) std::cout << " error: " << err << std::endl;
+        }
+        if (iter % 10 == 1) std::cout << ".";
         y_u = y_ret;
-        if (FLAGS_eps > err) break;
+        if (eps > err) {
+            std::cout << std::endl << iter << " iteration done." << std::endl;
+            break;
+        }
     }
-    LOG(INFO) << "iteration: " << iteration << " done.";
+
+    return true;
+}
+bool LP::write (const std::string filename)
+{
+
+}
+void LP::show ()
+{
+    std::cout << "========================================" << std::endl;
     showLabels(y_l, y_u, L, U, C, labeled_nodes, unlabeled_nodes, node_index_v, labels);
+}
+int main (int argc, char** argv)
+{
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    google::InitGoogleLogging(argv[0]);
+
+    graph::Matrix trans_mat;
+    graph::Matrix norm_trans_mat;
+    graph::Labels labels;
+
+    LP lprop(1.0e-10);
+    lprop.read(FLAGS_inputMatrix, FLAGS_inputLabels);
+    lprop.prop(FLAGS_iteration);
+    lprop.write("output");
+    lprop.show();
 }
